12d09e8a5ca8e0e821c8f02fd506bee96e61b004
Ulrich Habel <espero7757@gmx.net>
2015-01-08 13:52:42 +0000

    c_hostware_to_10MHz_Reference - create new MinGW branch

---
+++ Src/hostware_MinGW/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: 24.12.2014
+ *      Author: espero
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void openDevice(bool isReopening);
+void closeDevice();
+
+#endif /* MAIN_H_ */
