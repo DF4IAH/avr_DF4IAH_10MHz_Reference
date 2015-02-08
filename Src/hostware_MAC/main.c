518ff6826500d8d77e68fffcb8abd8b7f69e475d
Ulrich Habel <espero7757@gmx.net>
2015-02-08 17:41:27 +0000

    c_hostware_to_10MHz_Reference - Mac: isSend removed

--- Src/hostware_MAC/main.c
+++ Src/hostware_MAC/main.c
@@ -116,7 +116,6 @@ int main(int argc, char **argv)
 	 * debugging FIRMWARE code only
 	 */
 	{
-		const uint8_t isSend = false;
 		const uchar bufferTestIn[3] = { '1', '2', '3' };
 
 #ifdef TEST_RINGBUFFER
@@ -125,15 +124,15 @@ int main(int argc, char **argv)
 
 			for (;;) {
 				/* pull data */
-				if (fw_getSemaphore(isSend)) {
-					uint8_t retLen = fw_ringBufferPull(isSend, bufferTestOut1, (uint8_t) sizeof(bufferTestOut1));
-					fw_freeSemaphore(isSend);
+				if (fw_getSemaphore(false)) {
+					uint8_t retLen = fw_ringBufferPull(false, bufferTestOut1, (uint8_t) sizeof(bufferTestOut1));
+					fw_freeSemaphore(false);
 				}
 
 				/* push data */
-				if (fw_getSemaphore(isSend)) {
-					uint8_t retLen = fw_ringBufferPush(isSend, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
-					fw_freeSemaphore(isSend);
+				if (fw_getSemaphore(false)) {
+					uint8_t retLen = fw_ringBufferPush(false, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
+					fw_freeSemaphore(false);
 				}
 			}
 		}
@@ -146,43 +145,43 @@ int main(int argc, char **argv)
 
 			/* STEP 1 */
 			/* push */
-			if (fw_getSemaphore(isSend)) {
-				retLen = fw_ringBufferPush(isSend, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
-				fw_freeSemaphore(isSend);
+			if (fw_getSemaphore(false)) {
+				retLen = fw_ringBufferPush(false, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
+				fw_freeSemaphore(false);
 			}
 
 			/* pull */
-			if (fw_getSemaphore(isSend)) {
-				retLen = fw_ringBufferPull(isSend, bufferTestOut2, (uint8_t) sizeof(bufferTestOut2));
-				fw_freeSemaphore(isSend);
+			if (fw_getSemaphore(false)) {
+				retLen = fw_ringBufferPull(false, bufferTestOut2, (uint8_t) sizeof(bufferTestOut2));
+				fw_freeSemaphore(false);
 			}
 			printf("TEST_SEMAPHORE - STEP1 - RESULT: retLen=%d, '%s'\n", retLen, bufferTestOut2);
 
 			/* STEP 2 */
 			/* pushing ... */
-			if (fw_getSemaphore(isSend)) {
-				retLen = fw_ringBufferPush(isSend, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
-			// fw_freeSemaphore(isSend);	// <-- "FUNCTION ABOVE NOT FINISHED YET"
+			if (fw_getSemaphore(false)) {
+				retLen = fw_ringBufferPush(false, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
+			// fw_freeSemaphore(false);	// <-- "FUNCTION ABOVE NOT FINISHED YET"
 			}
 
 			/* ... during second instance tries to push */
-			if (fw_getSemaphore(isSend)) {
-				retLen = fw_ringBufferPush(isSend, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
-				fw_freeSemaphore(isSend);
+			if (fw_getSemaphore(false)) {
+				retLen = fw_ringBufferPush(false, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
+				fw_freeSemaphore(false);
 			} else {
-				fw_ringBufferPushAddHook(isSend, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
+				fw_ringBufferPushAddHook(false, bufferTestIn, (uint8_t) sizeof(bufferTestIn));
 			}
 
 			/* ... now the first instances completes */
-			// if (fw_getSemaphore(isSend)) {
-			//	retLen = fw_ringBufferPush(isSend, bufferTestIn, sizeof(bufferTestIn));
-				fw_freeSemaphore(isSend);	// <-- this completes the function now
+			// if (fw_getSemaphore(false)) {
+			//	retLen = fw_ringBufferPush(false, bufferTestIn, sizeof(bufferTestIn));
+				fw_freeSemaphore(false);	// <-- this completes the function now
 			// }
 
 			/* ... look, what we get in return */
-			if (fw_getSemaphore(isSend)) {
-				retLen = fw_ringBufferPull(isSend, bufferTestOut2, (uint8_t) sizeof(bufferTestOut2));
-				fw_freeSemaphore(isSend);
+			if (fw_getSemaphore(false)) {
+				retLen = fw_ringBufferPull(false, bufferTestOut2, (uint8_t) sizeof(bufferTestOut2));
+				fw_freeSemaphore(false);
 			}
 			printf("TEST_SEMAPHORE - STEP2 - RESULT: retLen=%d, '%s'\n", retLen, bufferTestOut2);
 		}
