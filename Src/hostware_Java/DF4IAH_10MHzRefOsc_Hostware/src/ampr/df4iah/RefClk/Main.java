dac1df0536f311df2c457ae905055ab7edfccffa
Ulrich Habel <espero7757@gmx.net>
2015-06-13 05:01:18 +0000

    avr_DF4IAH_10MHz_Reference - hostware_Java added (2)

---
+++ Src/hostware_Java/DF4IAH_10MHzRefOsc_Hostware/src/ampr/df4iah/RefClk/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ampr.df4iah.RefClk;
+
+/**
+ * @author DF4IAH
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("DF4IAH - 10 MHz Reference Oscillator - Hostware");
+	}
+}
