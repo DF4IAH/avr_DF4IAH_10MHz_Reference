84fb7b58b7adb7e21b1fa943907cc771b0b5b0c5
Ulrich Habel <espero7757@gmx.net>
2017-03-14 22:13:38 +0100

    c_hostware_Ubuntu - color scheme modified

--- Src/hostware_Ubuntu/terminal.c
+++ Src/hostware_Ubuntu/terminal.c
@@ -176,10 +176,10 @@ static void ncurses_init(WINDOW** win_rxborder, WINDOW** win_rx, WINDOW** win_tx
 		start_color();
 		use_default_colors();
 		init_pair(E_COLOR_PAIR_TITLE,			COLOR_WHITE,  COLOR_RED);
-		init_pair(E_COLOR_PAIR_SEND_MAIN,		COLOR_BLUE,  511);
-		init_pair(E_COLOR_PAIR_SEND_GPS,		COLOR_BLUE,   15);
-		init_pair(E_COLOR_PAIR_RCV_MAIN,		COLOR_BLACK,  511);
-		init_pair(E_COLOR_PAIR_RCV_GPS,			COLOR_BLACK,   15);
+		init_pair(E_COLOR_PAIR_SEND_MAIN,		COLOR_RED,    511);
+		init_pair(E_COLOR_PAIR_SEND_GPS,		COLOR_RED,    COLOR_WHITE);
+		init_pair(E_COLOR_PAIR_RCV_MAIN,		COLOR_BLUE,   511);
+		init_pair(E_COLOR_PAIR_RCV_GPS,			COLOR_BLUE,   COLOR_WHITE);
 		init_pair(E_COLOR_PAIR_DEBUGGING_IN,	COLOR_YELLOW, COLOR_BLACK);
 		init_pair(E_COLOR_PAIR_DEBUGGING_OUT,	COLOR_WHITE,  COLOR_BLACK);
 	}
--- Src/hostware_Ubuntu/terminal.h
+++ Src/hostware_Ubuntu/terminal.h
@@ -20,31 +20,11 @@
 typedef unsigned char  uchar;
 
 
-/* -- 8< --  RINGBUFFERS */
 int ringbuffer_fw_ringBufferPush(char isSend, uchar inData[], int len);
 int ringBufferPull(char isSend, uchar outData[], int size);
 
-//static void usb_buffer_controlOut(uchar inLine[], int len);
-//static int usb_buffer_controlIn(uchar outLine[], int size);
-
 void usb_do_transfers();
 
-
-/* -- 8< --  USB */
-//static void usb_controlOut(char inLine[], int len);
-//static int usb_controlIn(char outLine[], int size);
-
-
-/* -- 8< --  NCURSES */
-//static void ncurses_init(WINDOW** win_rxborder, WINDOW** win_rx, WINDOW** win_tx);
-//static void ncurses_finish(WINDOW** win_rxborder, WINDOW** win_rx, WINDOW** win_tx);
-//static void ncurses_rx_print(WINDOW** win_rx, const char string[], int colorPair, attr_t attributes);
-//static void ncurses_tx_clear(WINDOW** win_tx);
-//static void ncurses_tx_print(WINDOW** win_tx, const char string[]);
-//static void ncurses_update(WINDOW** win_rxborder, WINDOW** win_rx, WINDOW** win_tx);
-
-
-/* -- 8< --  TERMINAL - LOOP */
 void terminal();
 
 
