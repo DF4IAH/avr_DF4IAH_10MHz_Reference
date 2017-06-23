76804f9e2479908d9583dcdcb9015c316210b52f
Ulrich Habel <espero7757@gmx.net>
2017-06-24 00:17:01 +0200

    SW_V2.x.1 - TWI communication enhanced for both standard display and Smart-LCD (2)

--- DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/df4iah_fw_main.h
+++ DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/df4iah_fw_main.h
@@ -14,7 +14,7 @@
 
 /* VERSION: YYM, MDD */
 #define VERSION_HIGH										170
-#define VERSION_LOW											623
+#define VERSION_LOW											624
 
 
 /* GPS NMEA */
@@ -94,21 +94,19 @@
 
 typedef struct main_bf_struct
 {
-     uint8_t  mainIsLcdAttached;
-     uint8_t  mainIsSmartAttached;
-
      uint8_t  mainIsAFC										: 1;
      uint8_t  mainIsAPC										: 1;
      uint8_t  mainIsTimerTest								: 1;
      uint8_t  mainIsSerComm									: 1;
      uint8_t  mainIsUsbCommTest 							: 1;
      uint8_t  mainStopAvr		 							: 1;
-     uint8_t  mainStackCheck								: 1;
-     uint8_t  mainReserved01								: 1; // fill to 8 bits
+     uint8_t  mainIsLcdAttached								: 1;
+     uint8_t  mainIsSmartAttached							: 1;
 
+     uint8_t  mainStackCheck								: 1;
      uint8_t  mainHelpConcatNr								: 4;
      uint8_t  mainLcdLedMode								: 3;
-     uint8_t  mainReserved11								: 1; // fill to 8 bits
+//     uint8_t  mainReserved11								: 0; // fill to 8 bits
 } main_bf_t;
 
 enum REFCLK_STATE_t {
