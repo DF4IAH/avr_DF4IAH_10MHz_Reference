5eeff0eb25b53edda4257a9d8fa131f809973769
Ulrich Habel <espero7757@gmx.net>
2017-12-01 15:14:57 +0100

    SW_V2.x.1 - Programmer script - no EEPROM update, FLASH only

--- Images/Program_ATmega-328P_Application.sh
+++ Images/Program_ATmega-328P_Application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#avrdude -pm328p -cdragon_pp -D -Ueeprom:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.eep:a -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
-avrdude -pm328p -cusbasp-clone -D -Ueeprom:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.eep:a -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
+#avrdude -pm328p -cdragon_pp -D -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
+avrdude -pm328p -cusbasp-clone -D -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
 
 echo "==="
 echo "Programming done." 
