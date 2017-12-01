545b72936a2b861483500efeb86da96785b17484
Ulrich Habel <espero7757@gmx.net>
2017-12-01 16:33:02 +0100

    SW_V2.x.1 - Programmer script - more scripts added

---
+++ Images/Program_ATmega-328P_Application_FLASH_Dragon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+avrdude -pm328p -cdragon_pp -D -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
+
+echo "==="
+echo "Programming done." 
+echo
+
--- Images/Program_ATmega-328P_Application_FLASH_InCircuit.sh
+++ Images/Program_ATmega-328P_Application_FLASH_InCircuit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#avrdude -pm328p -cdragon_pp -D -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
 avrdude -pm328p -cusbasp-clone -D -Uflash:w:../DF4IAH_10MHz_Reference_Appl/DF4IAH_10MHz_Reference_Appl/Release/DF4IAH_10MHz_Reference_Appl.hex:a
 
 echo "==="
